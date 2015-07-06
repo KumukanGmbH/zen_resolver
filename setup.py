6faf93b3bae6525c3a2ba1ae1167b2d8ea691d61
Ross Crawford-d'Heureuse <ross.crawford@kumukan.com>
2015-07-06 09:31:41 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup
+
+setup(
+    name = "zen_resolver",
+    version = "0.0.1",
+    author = "Ross Crawford-d'Heureuse",
+    author_email = "ross.crawford@kumukan.com",
+    description = ("Script for closing zendesk tickets when we get pdfs"),
+    license = "GPL",
+    keywords = "",
+    url = "https://github.com/KumukanGmbH/zen_resolver",
+    packages=['zdesk'],
+    install_requires = [
+        'zdesk',  # must refer to package version explicitly **required**
+        'requests',
+        'pyOpenSSL',
+    ]
+)
